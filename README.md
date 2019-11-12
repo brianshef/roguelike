646f745dcf0db75f578604d7773d759c2133ae10
Brian Shef <brian.shef@rewardStyle.com>
2019-11-12 08:51:58 -0600

    Add termloop

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # roguelike
 A roguelike written in Go
 
+## Requirements
+
+Uses [termloop](https://github.com/JoelOtter/termloop), which can be installed simply with:
+
+```
+go get -u github.com/JoelOtter/termloop
+```
+
 ## Running roguelike
 
 Enter the following command to build the executable:
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import tl "github.com/JoelOtter/termloop"
 
 func main() {
-	fmt.Println("roguelike")
+	game := tl.NewGame()
+	level := tl.NewBaseLevel(tl.Cell{
+		Bg: tl.ColorGreen,
+		Fg: tl.ColorBlack,
+		Ch: 'v',
+	})
+	level.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
+	game.Screen().SetLevel(level)
+	game.Start()
 }
